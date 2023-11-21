13284f5032feeb5a7bbe2724abe0515a5cf5c36b
timothyas <timsmith204@utexas.edu>
2023-11-21 13:39:55 -0700

    cice6 dataset looks good to go

--- ufs2arco/cice6dataset.py
+++ ufs2arco/cice6dataset.py
@@ -33,32 +33,71 @@ class CICE6Dataset(UFSDataset):
     def open_dataset(self, cycles: datetime, fsspec_kwargs=None, **kwargs):
         xds = super().open_dataset(cycles, fsspec_kwargs, **kwargs)
 
+        # Before adding time variables, promote some coordinates and drop time from their dims
+        xds = self._set_coords(xds)
+        xds = self._swap_dims(xds)
+
         # Deal with time
         # make cftime to be consistent with MOM and FV3
         xds["cftime"] = self._time2cftime(xds["time"])
         xds["ftime"] = self._time2ftime(xds["time"], cycles)
         xds = xds.set_coords(["time", "cftime", "ftime"])
+        return xds
 
-        ## Swap meaningless logical indices for values, where it makes sense (basically, not ni,nj)
-        ## Note we can only do this for time_bounds b/c ds is squeezed
-        #swap = {
-        #    "nc": "NCAT",
-        #    "nkaer": "VGRDa",
-        #    "nkice": "VGRDi",
-        #    "nksnow": "VGRDs",
-        #    "d2": "time_bounds",
-        #}
-        #for org, new in swap.items():
-        #    if new in xds and xds[new].ndim>0:
-        ##        for d in ["time", "member"]:
-        ##            if d in xds[new].dims:
-        ##                xds[new] = xds[new].isel({d:0})
-        #        xds = xds.swap_dims({org: new})
-        #    if org in xds:
-        #        xds = xds.drop(org)
+    def _set_coords(self, xds):
+        """These known coordinates show up in the dataset as if they varied in time, fix that"""
 
-        ## This one gets squeezed out and is for some reason problematic?
-        #if xds["VGRDs"].ndim == 0:
-        #    xds = xds.drop("VGRDs")
+        coords = [
+            "nc"
+            "nkaer",
+            "nkice",
+            "nksnow",
+            "d2",
+			"NCAT",
+			"VGRDa",
+			"tmask",
+			"umask",
+			"nmask",
+			"emask",
+			"blkmask",
+			"tarea",
+			"uarea",
+			"narea",
+			"earea",
+			"dxn",
+			"dyn",
+			"dxe",
+			"dye",
+			"ANGLE",
+			"ANGLET",
+		]
+
+        for key in coords:
+            if key in xds.data_vars:
+                if "time" in xds[key].dims:
+                    xds[key] = xds[key].isel({"time": 0}).drop("time")
+                xds = xds.set_coords(key)
+
+        return xds
+
+    def _swap_dims(self, xds):
+
+        # Swap meaningless logical indices for values, where it makes sense (basically, not ni,nj)
+        # Note we can only do this for time_bounds b/c ds is squeezed
+        swap = {
+            "nc": "NCAT",
+            "nkaer": "VGRDa",
+            "nkice": "VGRDi",
+            "nksnow": "VGRDs",
+            "d2": "time_bounds",
+        }
+        for org, new in swap.items():
+            if new in xds:
+                xds = xds.swap_dims({org: new})
+
+            # This last if statement is necessary because some of the dimensions (e.g., nc)
+            # are not even in the dataset, they are undefined.
+            if org in xds:
+                xds = xds.drop(org)
 
         return xds
