91a3cd4c09246ecc5943d4652e6a4bc7b52fb873
timothyas <smith.timothy.andrew@gmail.com>
2024-07-22 22:29:54 +0000

    add pressure interpolation code

--- ufs2arco/__init__.py
+++ ufs2arco/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .cice6dataset import CICE6Dataset
-from .fv3dataset import FV3Dataset
+from .fv3dataset import FV3Dataset, Layers2Pressure
 from .mom6dataset import MOM6Dataset
 from .regrid import MOM6Regridder, CICE6Regridder
 from .timer import Timer
