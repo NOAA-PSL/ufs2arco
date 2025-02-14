1135c173c6d440121385b1958d34b903a3db723c
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-02-14 10:28:20 -0800

    updated chunksizes, write path, debug queue

--- examples/gefs/submit_one.sh
+++ examples/gefs/submit_one.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH -J gefs-1.00
-#SBATCH -o /global/cfs/cdirs/m4718/timothys/gefs/one-degree/slurm/%j.out
-#SBATCH -e /global/cfs/cdirs/m4718/timothys/gefs/one-degree/slurm/%j.err
+#SBATCH -o /global/cfs/cdirs/m4718/timothys/gefs/one-degree/slurm/one_degree.%j.out
+#SBATCH -e /global/cfs/cdirs/m4718/timothys/gefs/one-degree/slurm/one_degree.%j.err
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=128
 #SBATCH --cpus-per-task=2
-#SBATCH --qos=regular
+#SBATCH --qos=debug
 #SBATCH --account=m4718
 #SBATCH --constraint=cpu
-#SBATCH -t 06:00:00
+#SBATCH -t 00:30:00
 
 conda activate /global/common/software/m4718/timothys/graphufs
 export PYTHONPATH=~/ufs2arco
